5dcbe97ee452dc66a80a3ae3f8afa4eae4a7ac23
vitoUwu <32278696+vitoUwu@users.noreply.github.com>
2022-10-02 17:10:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const { default: axios } = require('axios');
 
 const logData = async () => {
 	const apuracoes = await axios("https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json")
-		.then(res => res.data);
-	console.table(apuracoes.cand.map(cand => ({ name: cand.nm, votos: cand.vap, porcentagem: cand.pvap })));
+		.then(res => res.data.cand.map(cand => ({ name: cand.nm, votos: cand.vap, porcentagem: cand.pvap })));
+	console.table(apuracoes);
 	setTimeout(logData, 60000 * 2);
 } 
 
